f338bc426f662712cc84d2515fdf1e8a0bc9bcc1
Abdelrahman Mohammad Al Daour <Daour.995@outlook.com>
2021-05-23 16:36:42 +0300

    finished lab06

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
-export default App;
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchedCity: '',
+      chosenCity: '',
+      displayMap: false,
+      errorMessage: false
+    }
+  }
+
+    updateCityName = (event) => {
+      this.setState({
+        searchedCity: event.target.value
+      })
+      console.log(this.state.searchedCity);
+
+    }
+
+    updateLocation = async (event) => {
+      event.preventDefault();
+
+      let locationURL = `https://eu1.locationiq.com/v1/search.php?key=pk.c2cc49511f7e125dfbd3bdab371cb62a&q=${this.state.searchedCity}&format=json`;
+
+
+      try {
+        let cityData = await axios.get(locationURL)
+
+        this.setState({
+          chosenCity: cityData.data[0],
+          displayMap: true,
+
+
+        })
+        console.log(this.state.chosenCity);
+
+      }
+      catch {
+        this.setState({
+          displayMap: false,
+          errorMessage: true,
+        })
+
+      }
+    }
+
+
+    render() {
+      return (
+        <>
+          <h1>City Explorer</h1>
+
+          <form onSubmit={this.updateLocation}>
+
+            <input type='text' placeholder='Enter a city name' onChange={this.updateCityName} />
+            <input type='submit' value='Get Location' />
+
+            {/* <button onClick={this.updateCityName} /> */}
+          </form>
+
+          <h3>{this.state.chosenCity.display_name}</h3>
+
+          <p>{this.state.chosenCity.lon}</p>
+
+          <p>{this.state.chosenCity.lat}</p>
+
+          { this.state.displayMap &&  
+
+          <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.c2cc49511f7e125dfbd3bdab371cb62a&center=${this.state.chosenCity.lat},${this.state.chosenCity.lon}`} alt='' />
+          }
+
+
+          {this.state.errorMessage &&
+            <p>error in receving the data</p>
+          }
+
+          {/* 
+        <Form>
+           <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+         <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+         </Button>
+      </Form> */}
+        </>
+      );
+    }
+  }
+
+  export default App;
