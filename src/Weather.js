a21f25783c36e7d7818f0969829bd256b9dba5c7
Abdelrahman Mohammad Al Daour <Daour.995@outlook.com>
2021-05-26 15:55:57 +0300

    almost finished lab07

--- src/Weather.js
+++ src/Weather.js
@@ -1,41 +1,77 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 class Weather extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            cityData: ''
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         cityData: ''
 
-        }
+    //     }
 
-    }
-    getWeatherData = async () => {
-        let serverRoute = process.env.REACT_APP_SERVER;
-        // http://localhost:3001/test
-        // const url = `${serverRoute}/test`;
+    // }
+    // getWeatherData = async () => {
+    //     let serverRoute = process.env.REACT_APP_SERVER;
+    //     // http://localhost:3001/test
+    //     // const url = `${serverRoute}/test`;
 
-        // http://localhost:3001/getPoke?pokeName=bulbasaur
-        const url = `${serverRoute}/weather?city_name=${this.props.searchedCity}&lat=${this.props.lat}&lon=${this.props.lon}`;
+    //     // http://localhost:3001/getPoke?pokeName=bulbasaur
+    //     const url = `${serverRoute}/weather?city_name=${this.props.searchedCity}&lat=${this.props.lat}&lon=${this.props.lon}`;
 
-        // console.log(url);
-        const cityItem = await axios.get(url);
-        console.log(cityItem.data);
-        this.setState({
-            cityData: cityItem.data
-        })
+    //     // console.log(url);
+    //     const cityItem = await axios.get(url);
+    //     console.log(cityItem.data);
+    //     this.setState({
+    //         cityData: cityItem.data
+    //     })
 
-        
-    }
+
+    // }
+
+
 
     // renderDateDescription() {
     //     this.renderDateDescription(this.getWeatherData());
     // }
 
     render() {
-        return (
-            <p>{this.state.cityData.description} {this.renderDateDescription}</p>
+        // this.props.hornedData.map((item, idx) => {
 
+        //             return (
+        //                 <HornedBeast
+        //                     key={idx}
+        //                     title={item.title}
+        //                     imgsrc={item.image_url}
+        //                     description={item.description}
+        //                     renderingSelectedAnimal={this.props.renderingSelectedAnimal}
+
+        return (
+            // <>
+            //     {this.props.cityData.map((item, idx) => {
+            //         return (
+            //             <>
+            //                 <div key={idx}>
+            //                     <p>
+            //                         date={item.date}
+            //                     </p>
+            //                     <p>
+
+            //                         description={item.description}
+            //                     </p>
+            //                 </div>
+
+
+            //             </>
+            //         )
+            //     })
+            //     }
+            // </>
+
+            <>
+
+                <p> {this.props.date}  </p>
+                <p>{this.props.description}</p>
+            </>
         )
     }
 
